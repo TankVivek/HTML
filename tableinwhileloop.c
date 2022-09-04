92205f8523f0f20dc297e36519faec63e11c0b69
TankVivek <112782531+TankVivek@users.noreply.github.com>
2022-09-05 01:25:03 +0530

    Add files via upload

---
+++ tableinwhileloop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main () {
+
+   /* local variable definition */
+   int a = 2,n = 1;
+
+   /* while loop execution */
+   while(n <= 10)
+    {
+      printf("%d\n", a * n); n++;
+   }
+ 
+   return 0;
+}
